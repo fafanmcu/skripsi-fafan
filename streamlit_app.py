6dae4abb806203e37728eee7d76e716f98842c54
fafanmcu <fafanutama25@gmail.com>
2024-08-06 19:54:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Analisis Sentimen Pemindahan Ibu Kota Negara ke Ibu Kota Nusantara Menggunakan Metode Naive Bayes')
-
 st.info('Fafan Maulana Cahya Utama')
+
+data = pd.read_excel('ikn-maret-juni-dengan-label.xlsx')
+
+
+
