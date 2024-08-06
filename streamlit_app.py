eab6cafcaa9896b999c85a94bbfac4db68af3331
fafanmcu <fafanutama25@gmail.com>
2024-08-06 20:08:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,6 @@ mapping = {1: 'Positif', 2: 'Negatif'}
 # mengubah nilai kolom dan menghapus sentimen yang kosong
 df = data.dropna(subset=['Sentimen'])
 df['Sentimen'] = df['Sentimen'].map(mapping)
+# mengurutkan nomer index
 df = df.reset_index(drop=True)
 df
