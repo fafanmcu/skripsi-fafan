136e0f47147e989f4cdcdf2d854c6f7ca5c7382c
fafanmcu <fafanutama25@gmail.com>
2024-08-10 20:57:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,10 @@ df = df.drop_duplicates(subset=['Stemming'])
 df
 
 # Insert containers separated into tabs:
->>> tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
->>> tab1.write("this is tab 1")
->>> tab2.write("this is tab 2")
+tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
+tab1.write("this is tab 1")
+tab2.write("this is tab 2")
 
 # You can also use "with" notation:
->>> with tab1:
->>>   st.radio('Select one:', [1, 2])
+with tab1:
+st.radio('Select one:', [1, 2])
