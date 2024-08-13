18d851efede4c3075153873c8ac76fad352c8bcb
fafanmcu <fafanutama25@gmail.com>
2024-08-13 21:20:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,15 +23,3 @@ df
 tab1, tab2 = st.tabs(["Tab 1", "Tab2"])
 tab1.write("this is tab 1")
 tab2.write("this is tab 2")
-
-# You can also use "with" notation:
-with tab1:
-  st.radio('Select one:', [1, 2])
-
-# Insert a chat message container.
-with st.chat_message("user"):
-  st.write("Hello 👋")
-  st.line_chart(np.random.randn(30, 3))
-
-# Display a chat input widget.
-st.chat_input("Say something")
