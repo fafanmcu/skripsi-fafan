2bed7197a99d2428b5f72281b7332534b87b9050
fafanmcu <fafanutama25@gmail.com>
2024-08-16 12:00:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,12 +72,12 @@ with nav3:
     pos_df = df_selection[df_selection['Sentimen'] == 'Positif']
     neg_df = df_selection[df_selection['Sentimen'] == 'Negatif']
         
-    if not neg_df.empty:
+    if not pos_df.empty:
         color = ['#e14b32']
         fig_sentiment.add_trace(go.Pie(labels=['Negatif'], values=neg_df['Sentimen'].value_counts(), 
                                         marker_colors=color, textinfo='label+percent', 
                                         hoverinfo='label+value', hole=0.3))
-    if not pos_df.empty:
+    if not neg_df.empty:
         color = ['#3ca9ee']
         fig_sentiment.add_trace(go.Pie(labels=['Positif'], values=pos_df['Sentimen'].value_counts(), 
                                         marker_colors=color, textinfo='label+percent', 
