b8c9ec548491c6d1d36c2f779fb02d195c28a5c0
fafanmcu <fafanutama25@gmail.com>
2024-08-16 16:07:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -139,14 +139,14 @@ def print_metrics(y_test, y_pred, fold, title_suffix):
     st.write("**Classification Report:**")
     st.text(classification_report(y_test, y_pred))
 
-    plt.figure(figsize=(4, 3))  # Smaller figure size
+    plt.figure(figsize=(3, 2))  # Further reduce figure size
     sns.heatmap(cm, annot=True, fmt='d', cmap='viridis', xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'],
-                annot_kws={"size": 8}, cbar_kws={"shrink": .6})  # Adjust text size
+                annot_kws={"size": 8}, cbar_kws={"shrink": .7})  # Reduce text and color bar size
     plt.title(f'Confusion Matrix Fold {fold} - {title_suffix}', fontsize=10)
     plt.xlabel('Predicted label', fontsize=8)
     plt.ylabel('True label', fontsize=8)
-    plt.xticks(fontsize=6)
-    plt.yticks(fontsize=6)
+    plt.xticks(fontsize=7)
+    plt.yticks(fontsize=7)
     st.pyplot(plt)
 
 # Tabs for TF and TF-IDF
