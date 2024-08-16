19540d34b7b1f54c4b051b61e2de861d6de5e3fb
fafanmcu <fafanutama25@gmail.com>
2024-08-16 14:03:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ with nav4:
     st.plotly_chart(fig_tgl, use_container_width=True)
 
 st.markdown("""---""")
-kf_fold = st_selection("Pilih Jumlah Lipatan")
+kf_fold = st.selection("Pilih Jumlah Lipatan")
 tab3, tab4 = st.tabs(["TF, TF-IDF"])
 with tab3:
     st.write("TF")
